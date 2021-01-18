c15c27761243558c01d07c0ae3fda7ffa92af75a
Ichthyological Dev <ichthyologicaldev@gmail.com>
2021-01-18 09:43:23 -0800

    add solution

--- index.js
+++ index.js
@@ -15,53 +15,67 @@ let timeAsDev = 4;
 
 IMPORTANT
 You should have the index.html file open in your browser as you work through this
-exercise. If you aren't sure what this means, you should watch the <INSERT LINK HERE> 
-video before continuing.
+exercise. If you aren't sure what this means, you should watch the How to Edit Code 
+video: https://youtu.be/vkWDQzl_x3s before continuing.
 
 If you currently doubt your ability to create variables, I hope this exercise casts that doubt away.
 */
 
 //REQUIREMENT 1: How many days have you been a developer?
 //TODO: Instantiate a variable, timeAsDev, with your answer. Make sure to use a keyword that allows the value to be updated later!
+let timeAsDev = 5000;
 
 //REQUIREMENT 2: What is the genus of the Largemouth Bass?
 //TODO: Insantiate a variable, bassGenus, with your answer. (Google it!)
+const bassGenus = 'Micropterus';
 
 //REQUIREMENT 3: True or false, you should use the "var" keyword instead of "const" or "let" to create variables.
 //TODO: Instantiate a variable, isVarPreferred, with your answer.
+const isVarPreferred = false;
 
 //REQUIREMENT 4: Roughly how many gallons of water are on Earth?
 //TODO: Instantiate a variable, gallonsWaterOnEarth, with your answer. (Google it!)
+const gallonsWaterOnEarth = 326000000000000000000n;
 
 //REQUIREMENT 5: Instantiate a variable, variablesThusFar, with the total number of variables you made before this question.
 //TODO: Make sure to use a keyword that allows the value to be updated later!
+let variablesThusFar = 4;
 
 //REQUIREMENT 6: How many feet are in a fathom?
 //TODO: Instantiate a variable, feetInFathom, with your answer
+const feetInFathom = 6;
 
 //REQUIREMENT 7: What is the plural of fish if we're talking about different species?
 //TODO: Instantiate a variable, fishPluralForm, with your answer.
+const fishPluralForm = 'fishes';
 
 //REQUIREMENT 8: Update timeAsDev to represent how many days you've been a developer at the end of next week.
 //TODO: Reassign timeAsDev with your answer.
+timeAsDev = 5007;
 
 //REQUIREMENT 9: True or false, instantiating a variable with the let keyword allows you to reassign the value later.
 //TODO: Instantiate a variable, canChangeLet, with your answer.
+const canChangeLet = true;
 
 //REQUIREMENT 10: What is the default value of a variable in JavaScript?
 //TODO: Instantiate a variable, jsDefaultValue, with your answer.
+let jsDefaultValue;
 
 //REQUIREMENT 11: What's the difference between null and undefined?
 //TODO: Instantiate a variable, nullVsUndefined, with your answer.
+const nullVsUndefined = 'undefined is the placeholder value that JavaScript assigns. Null is the value that a developer would assign to represent the intentional lack of a value';
 
 //REQUIREMENT 12: If someone is signing up for a user account, and leaves a field blank, what value should you assign to it?
 //TODO: Instantiate a variable, userOmitted, with your answer.
+const userOmitted = null;
 
 //REQUIREMENT 13: Update variablesThusFar with the new number of variables you've made so far.
 //TODO: Reassign variablesThusFar with your answer. 
+variablesThusFar = 11;
 
 //REQUIREMENT 14: True or false, the stonefish gets its name because people use them as stepping stones.
 //TODO: Instantiate a variable, stonefishAreSteps, with your answer.
+const stonefishAreSteps = false;
 
 /*
 You've reached the end of this exercise! Keep swimming along!
